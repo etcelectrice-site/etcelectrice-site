fbd9635a63cd0dbf43222dec94912779012abfe7
etcelectrice-site <nazolus@gmail.com>
2025-10-17 22:07:02 +0300

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,5 @@
 import "../styles/globals.css";
 import { Navbar } from "../components/Navbar";
-import { Navbar } from "@/components/Navbar";
-
 export const metadata: Metadata = {
   title: "Euro Top Construct – Villamos kivitelezés, biztonsági rendszerek, napelemek",
   description: "Euro Top Construct: villamos kivitelezés 0,4 kV-ig, villámvédelem, biztonsági rendszerek, tűzjelző rendszerek, napelem (ON/OFF-grid).",
