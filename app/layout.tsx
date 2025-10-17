70a1d5bdafd24b69597208500b3d42683ae4f406
etcelectrice-site <nazolus@gmail.com>
2025-10-17 22:42:41 +0300

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,11 +3,8 @@ import * as React from "react";
 import type { Metadata } from "next";
 import { Navbar } from "../components/Navbar";
 
-export const metadata: Metadata = {
-  title: "Euro Top Construct – Villamos kivitelezés, biztonsági rendszerek, napelemek",
-  description:
-    "Euro Top Construct: villamos kivitelezés 0,4 kV-ig, villámvédelem, biztonsági rendszerek, tűzjelző rendszerek, napelem (ON/OFF-grid).",
-  metadataBase: new URL("https://www.etcelectrice.net"),
+export const metadata = {
+  ...
 };
 
 export default function RootLayout({
