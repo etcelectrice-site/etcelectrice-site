fd38ed842177d3fc6b3271c15acba7240d005b7e
etcelectrice-site <nazolus@gmail.com>
2025-11-19 17:32:08 +0200

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,11 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  i18n: {
-    locales: ["hu", "ro"],
-    defaultLocale: "hu",
-    // localeDetection-t egyszerűen kivesszük / nem használjuk
-  },
 };
 
 export default nextConfig;
